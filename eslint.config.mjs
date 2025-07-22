e35e50376c4e10a836bcdc72117d2dbd6b7cfc75
Jegannath <sjegannath56@gmail.com>
2025-07-22 17:37:50 +0530

    dunno1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",
 
   // Rules override MUST be separate
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
